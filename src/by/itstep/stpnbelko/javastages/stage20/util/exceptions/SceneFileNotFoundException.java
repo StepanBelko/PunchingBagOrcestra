f879103b10e9223a4321e44a3fa99a254195b39c
Stepan Belko <Stpn.Belko@gmail.com>
2022-06-21 18:47:49 +0300

    added SceneFileNotFoundException.java

---
+++ src/by/itstep/stpnbelko/javastages/stage20/util/exceptions/SceneFileNotFoundException.java
@@ -0,0 +1,10 @@
+package by.itstep.stpnbelko.javastages.stage20.util.exceptions;
+
+public class SceneFileNotFoundException extends Exception {
+    public SceneFileNotFoundException() {
+    }
+
+    public SceneFileNotFoundException(String message) {
+        super(message);
+    }
+}
